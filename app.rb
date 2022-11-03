5dc2ed65d8ebe09d0827b54855d617cbfd186fe2
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-03 14:24:54 -0400

    Add person UI

---
+++ app.rb
@@ -0,0 +1,157 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength
+
+require_relative 'src/book'
+require_relative 'src/student'
+require_relative 'src/teacher'
+
+class App
+  def initialize
+    @books = []
+    @teachers = []
+    @students = []
+    @next_id = 0
+  end
+
+  def run
+    puts 'Welcome to School Library App!'
+    options_case
+  end
+
+  def list_all_books
+    @books.each do |book|
+      puts "  #{book.title} by #{book.author}"
+      puts '  rented by:'
+      book.rentals.each do |rent|
+        puts "    #{rent.person.name} at #{rent.date}"
+      end
+    end
+  end
+
+  def add_book
+    print 'title: '
+    title = gets.chomp
+    print 'author: '
+    author = gets.chomp
+    @books.push(Book.new(title, author))
+  end
+
+  def list_all_persons
+    puts " ---- Students"
+    @students.each do |student|
+      puts "  #{student.name} #{student.age} years old #{student.can_use_services? ? 'and can use services' : '' }"
+    end
+    puts " ---- Teachers"
+    @teachers.each do |teacher|
+      puts "  #{teacher.name} has #{teacher.age} years old specialized in: #{teacher.specialization}"
+    end
+  end
+
+  def person_info_input
+
+    print "Age: "
+    age = gets.chomp
+
+    print "Name: "
+    name = gets.chomp
+
+    {age: age.to_i, name: name}
+  end
+
+  def add_person
+    print 'Do you want to create a student [1] or teacher [2] Input the Number: '
+    number = gets.chomp.downcase
+
+    case number
+    when "1" # STUDENT
+      person_info = person_info_input
+
+      print "Has parrent permission? [Y/N]: "
+      permission = gets.chomp.downcase == "y"
+
+      @students.push(
+        Student.new(
+          person_info[:age],
+          person_info[:name],
+          parent_permission: permission
+        )
+      )
+    when "2" # TEACHER
+      person_info = person_info_input
+
+      print "Specialization: "
+      specialization = gets.chomp
+
+      @teachers.push(
+        Teacher.new(
+          person_info[:age],
+          specialization,
+          person_info[:name],
+        )
+      )
+
+    else
+      "You gave me #{option} -- I have no idea what to do with that.\n"
+    end
+
+    # @books.push(Book.new(title, author))
+  end
+
+  private
+
+  def option_in
+    puts 'Please choose a option by enterin a number:'
+    puts "\n"
+    puts_options
+    option = gets.chomp.downcase
+    puts "\n"
+    option
+  end
+
+  def options_case
+    loop do
+      case option_in
+      when '1'
+        # CASE [1] List all books.
+        puts "List all books.\n"
+        list_all_books
+      when '2'
+        # CASE [2] List all people.
+        puts "List all people.\n"
+        list_all_persons
+      when '3'
+        # CASE [3] Create a person (teacher or student).
+        add_person
+        puts "Create a person (teacher or student).\n"
+      when '4'
+        # CASE [4] Create a book.
+        puts "Create a book.\n"
+        add_book
+      when '5'
+        # CASE [5] Create a rental.
+        puts "Create a rental.\n"
+      when '6'
+        # CASE [6] List all rentals for a given person id.
+        puts "List all rentals for a given person id.\n"
+      when '7'
+        # CASE [7] Exit
+        puts "Exit\n"
+        break
+      else
+        "You gave me #{option} -- I have no idea what to do with that.\n"
+      end
+    end
+  end
+
+  def puts_options
+    puts '[1] - List all books.'
+    puts '[2] - List all people.'
+    puts '[3] - Create a person (teacher or student).'
+    puts '[4] - Create a book.'
+    puts '[5] - Create a rental.'
+    puts '[6] - List all rentals for a given person id.'
+    puts '[7] - Exit'
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/MethodLength
--- src/teacher.rb
+++ src/teacher.rb
@@ -1,8 +1,10 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization = 'Unknown', name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  attr_reader :specialization
+  def initialize(age, specialization = 'Unknown', name = 'Unknown')
+    super(age, name, parent_permission: true)
+
     @specialization = specialization
   end
 
